c1fba01dd58f269d10f17ebcb9285fc36903b8e2
Harsh Jain <harsh@crispygam.es>
2014-10-10 10:34:17 +0530

    boom

---
+++ src/main/java/com/jarsj/PBXResourcesBuildPhase.java
@@ -0,0 +1,5 @@
+package com.jarsj;
+
+public class PBXResourcesBuildPhase {
+
+}
--- src/main/java/com/jarsj/XCodeProject.java
+++ src/main/java/com/jarsj/XCodeProject.java
@@ -1,6 +1,12 @@
 package com.jarsj;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+import com.dd.plist.ASCIIPropertyListParser;
+import com.dd.plist.NSDictionary;
 
 /**
  * This class represents an entire XCodeProject. It's initialize using a root folder, whose structure is
@@ -16,27 +22,38 @@ import java.io.File;
  * root.xcodeproj   
  *  - project.pbxproj [Important file]
  *  
- * The project.pbxproj file contains the entire structure, groups, frameworks, build settings etc.   
+ * The project.pbxproj file contains the entire structure, groups, frameworks, build settings etc. 
+ * http://www.monobjc.net/xcode-project-file-format.html explains the format in more detail.
  *  
  * @author harsh
  */
 public class XCodeProject {
+	private String name;
 	
+	private int archiveVersion;
+	private PBXProject rootObject;
+	private ArrayList<PBXBuildFile> buildFiles;
+	private ArrayList<PBXFileReference> fileReferences;
+	
+	// The root folder
 	private File root;
+	
 	
 	/**
 	 * Initialize with a folder. If folder is empty a new project is created with the name of the folder, else
 	 * existing project inside the folder is loaded.
 	 * 
 	 * @param folder
+	 * @throws ParseException 
+	 * @throws IOException 
 	 */
-	public XCodeProject(File folder) {
+	public XCodeProject(File folder) throws IOException, ParseException {
 		this.root = folder;
+		this.buildFiles = new ArrayList<>();
+		name = folder.getName();
+		parse();
 	}
-	
-	
-	
-	
+		
 	/**
 	 * Return name of this project.
 	 * 
@@ -59,4 +76,13 @@ public class XCodeProject {
 		
 	}
 	
+	private void parse() throws IOException, ParseException {
+		NSDictionary rootNode = (NSDictionary) ASCIIPropertyListParser.parse(new File(root, name + ".xcodeproj/project.pbxproj"));
+		System.out.println(rootNode.toXMLPropertyList());
+		
+	}
+	
+	public static void main(String[] args) throws IOException, ParseException {
+		XCodeProject project = new XCodeProject(new File("/Users/harsh/xcodeproj-java/empty"));
+	}
 }
